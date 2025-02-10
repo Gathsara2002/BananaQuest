5541902f6e45a20724aeb79fb41f3fe4b4550be2
Gathsara2002 <liviru2002mmg@gmail.com>
2025-02-10 22:42:28 +0530

    game win  ui updated

--- frontend/src/views/pages/win/WinComponent.jsx
+++ frontend/src/views/pages/win/WinComponent.jsx
@@ -5,6 +5,10 @@ const WinComponent = () => {
     return (
         <div className='win-wrapper'>
             <div className='win-bg-image'></div>
+            <div className='win-buttons'>
+                <div className='next-btn'></div>
+                <div className='home-btn'></div>
+            </div>
         </div>
     );
 };
