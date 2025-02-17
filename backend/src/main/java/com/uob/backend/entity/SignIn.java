518d2f3984834fb0263cb674a57eb3b1114d5b46
Gathsara2002 <liviru2002mmg@gmail.com>
2025-02-17 21:36:16 +0530

    create sign in entity

---
+++ backend/src/main/java/com/uob/backend/entity/SignIn.java
@@ -0,0 +1,25 @@
+package com.uob.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : Gathsara
+ * created : 2/17/2025 -- 9:31 PM
+ **/
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "sign_in")
+public class SignIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String email;
+    String password;
+
+}
