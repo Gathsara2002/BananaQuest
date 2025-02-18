36d2111d40631e9eacd24ed35781ffbb2fc83bbd
Gathsara2002 <liviru2002mmg@gmail.com>
2025-02-18 17:51:32 +0530

    add model mapper to project

--- backend/src/main/java/com/uob/backend/BackendApplication.java
+++ backend/src/main/java/com/uob/backend/BackendApplication.java
@@ -1,14 +1,21 @@
 package com.uob.backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
