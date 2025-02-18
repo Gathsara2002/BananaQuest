3b4b4556749eebc923e9a9c2cdb8a235b302a90b
Gathsara2002 <liviru2002mmg@gmail.com>
2025-02-18 17:50:40 +0530

    crete player controller

---
+++ backend/src/main/java/com/uob/backend/controller/PlayerController.java
@@ -0,0 +1,16 @@
+package com.uob.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 2/18/2025 -- 5:50 PM
+ **/
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/player")
+public class PlayerController {
+}
