342df944ed76611fb0593b3663eb6ead51c83074
Gathsara2002 <liviru2002mmg@gmail.com>
2025-02-18 17:13:34 +0530

    crete sign in service implementation

---
+++ backend/src/main/java/com/uob/backend/service/impl/SignInServiceImpl.java
@@ -0,0 +1,19 @@
+package com.uob.backend.service.impl;
+
+import com.uob.backend.repository.SignInRepository;
+import com.uob.backend.service.SignInService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : Gathsara
+ * created : 2/18/2025 -- 5:12 PM
+ **/
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SignInServiceImpl implements SignInService {
+    private final SignInRepository repository;
+}
