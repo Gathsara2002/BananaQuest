fe1e9cc3b8985a97fbff74264deb6ad63810fd6e
Gathsara2002 <liviru2002mmg@gmail.com>
2025-02-21 20:08:32 +0530

    create sign in service

---
+++ frontend/src/service/SignInService.js
@@ -0,0 +1,33 @@
+import BASE_URL from "../url/urlConfiguration.js";
+import axios from "axios";
+import log from "eslint-plugin-react/lib/util/log.js";
+
+export async function signInUser(signIn) {
+    try {
+        const response = await axios.post(`${BASE_URL}/sign-in/save`, signIn);
+        return response.data;
+    } catch (error) {
+        console.error('Sign-in Error:', error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
+
+export async function loginUser(login) {
+    try {
+        const response = await axios.post(`${BASE_URL}/sign-in/login`, login);
+        return response.data;
+    } catch (error) {
+        console.error('Login Error:', error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
+
+export async function getAllUsers() {
+    try {
+        const response = await axios.get(`${BASE_URL}/sign-in/get-all`);
+        return response.data;
+    } catch (error) {
+        console.error('Loading Error:', error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
