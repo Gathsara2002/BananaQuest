08a1db30d153421a8c66f1f5f5632dae679c6469
Gathsara2002 <liviru2002mmg@gmail.com>
2025-03-01 18:40:49 +0530

    leaderboard ui updated

--- frontend/src/views/pages/leaderboard/LeaderboardComponent.jsx
+++ frontend/src/views/pages/leaderboard/LeaderboardComponent.jsx
@@ -15,6 +15,13 @@ const LeaderboardComponent = () => {
             <div className='leaderboard-bg-image'>
                 <div className='leaderboard-content'>
                     <h3 className='leaderboard-topic'>LEADERBOARD</h3>
+                    <div className='leaderboard-score-container'>
+                        <div className='leaderboard-score-row'>
+                            <div className='leaderboard-score-rank'>1</div>
+                            <div className='leaderboard-score-user'>Gathsara</div>
+                            <div className='leaderboard-score-point'>1000</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='leaderboard-buttons'>
