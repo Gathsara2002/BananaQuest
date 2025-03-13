cc1215a89abe48ffeaba684a991df672eabf717c
Gathsara2002 <liviru2002mmg@gmail.com>
2025-03-13 17:26:41 +0530

    create my user detail service class for get users

--- backend/src/main/java/com/uob/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/uob/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.uob.backend.config;
 
+import com.uob.backend.service.impl.MyUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
